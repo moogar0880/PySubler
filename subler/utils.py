cca1c2514440a89d0da11436354129a39c55f167
moogar0880 <moogar@comcast.net>
2014-09-14 21:36:51 -0400

    Adding subler.utils module

---
+++ subler/utils.py
@@ -0,0 +1,17 @@
+"""Utilities for assisting with the writing/tagging of metadata"""
+import sys
+import subprocess
+
+__all__ = ['subler_executable']
+
+
+def subler_executable():
+    """Find a localized subler executable and return it's path. If no executable
+    can be found, None is returned
+    """
+    executable = subprocess.check_output('which SublerCLI', shell=True).strip()
+    if sys.version_info[0] == 3:
+        executable = executable.decode('UTF-8')
+    if executable == '':
+        return 'SublerCLI'
+    return executable
