7554e428a48d78d7b43fefb8e1219935866b38c8
moogar0880 <moogar0880@gmail.com>
2015-07-20 22:41:51 -0400

    Adding mock function

---
+++ tests/mocks.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""A Collection of mock utilities to assisst with testing"""
+
+
+def mock_tag(self):
+    """A simple mock for the Subler class's tag method that just returns the
+    tagger instance
+    """
+    return self
